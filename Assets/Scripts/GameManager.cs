e10c450a2df1a612ab879ae4f8dd20674caf5d25
nisafitrianti <you@example.com>
2021-01-13 11:28:56 +0700

    bug in miss score and hit effect

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public BeatScroller theBS;
+
+    public static GameManager instance;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int scoreGoodNote = 125;
+    public int scorePerfectNote = 150;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        scoreText.text = "Score : 0";
+        currentMultiplier = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit On Time");
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier : x" + currentMultiplier;
+
+        //currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score : " + currentScore; 
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scoreGoodNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += scorePerfectNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void Notemissed()
+    {
+        Debug.Log("Note Missed");
+    }
+
+
+}
